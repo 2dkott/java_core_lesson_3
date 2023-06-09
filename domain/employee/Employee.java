17d3d6246cbad4948f9b0b24c961b8ed65959239
kot2d <kot2d@outlook.com>
2023-06-09 22:44:27 +0200

    add comparable to class

--- domain/employee/Employee.java
+++ domain/employee/Employee.java
@@ -2,8 +2,30 @@ package domain.employee;
 
 import domain.payments.Payment;
 
-public abstract class Employee {
+import java.util.UUID;
+
+public abstract class Employee implements Comparable<Employee>{
+
+    final private String employeeId;
+    final private Names names;
 
 
+    public Employee(Names names){
+        this.employeeId = UUID.randomUUID().toString();
+        this.names = names;
+    }
     abstract public Payment calcMonthSalary(Payment payment);
+
+    @Override
+    public int compareTo(Employee o) {
+        return names.compareTo(o.getNames());
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public Names getNames() {
+        return names;
+    }
 }
