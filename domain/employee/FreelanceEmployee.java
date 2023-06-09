51fd4dc383a76157ee04d30781398ca346bc9ece
kot2d <kot2d@outlook.com>
2023-06-09 22:46:44 +0200

    fixed classes with constructor

--- domain/employee/FreelanceEmployee.java
+++ domain/employee/FreelanceEmployee.java
@@ -3,7 +3,11 @@ package domain.employee;
 import domain.payments.Payment;
 import domain.payments.PaymentCalc;
 
-public class FreelanceEmployee extends Employee{
+public class FreelanceEmployee extends Employee {
+
+    public FreelanceEmployee(Names names) {
+        super(names);
+    }
 
     @Override
     public Payment calcMonthSalary(Payment payment) {
--- domain/employee/FullTimeEmployee.java
+++ domain/employee/FullTimeEmployee.java
@@ -5,6 +5,9 @@ import domain.payments.PaymentCalc;
 
 public class FullTimeEmployee extends Employee {
 
+    public FullTimeEmployee(Names names) {
+        super(names);
+    }
     @Override
     public Payment calcMonthSalary(Payment payment) {
         return PaymentCalc.calcNetFullTimeSalary(payment);
